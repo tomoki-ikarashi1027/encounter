d728aca3a0bc67dacbfae4d8acbe5043b69b2e15
tomoki <lb.s0172@gmail.com>
2021-01-24 16:08:15 +0900

    [Add]update_without_current_passwordメソッド

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-
+  validates :self_introduction, length: { maximum: 500 }
   enum gender: {man: 0, woman: 1}
+
+  def update_without_current_password(params, *options)
+
+    if params[:current_password].blank?
+      params.delete(:current_password)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
 end
