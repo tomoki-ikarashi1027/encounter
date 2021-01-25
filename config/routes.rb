350c85d2ac438b6f1d466212f4fb58f0f1a73870
tomoki <lb.s0172@gmail.com>
2021-01-25 21:47:53 +0900

    [Add]Reactionモデル

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users,
-  controllers: {registrations: 'registrations' }
+  controllers: {
+    registrations: 'registrations',
+    sessions: 'sessions'
+   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tops#index'
   resources :users, only: [:index, :show]
