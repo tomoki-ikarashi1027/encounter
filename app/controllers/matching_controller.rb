8c7e93353a2c4fc3fae8fb807b7e553cd50c0191
tomoki <lb.s0172@gmail.com>
2021-01-27 01:54:16 +0900

    [Add]ChatRoomsController

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -2,8 +2,14 @@ class MatchingController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    got_reaction_user_ids = Reaction.where(to_user_id: current_user.id,state: 'like').pluck(:from_user_id)
-    @match_users = Reaction.where(to_user_id: got_reaction_user_ids, from_user_id: current_user.id, status: 'like').map(&:to_user)
-    @user = User.find(current.id)
+    # got_reaction_user_ids = Reaction.where(to_user_id: current_user.id,state: 'like').pluck(:from_user_id)
+    # @match_users = Reaction.where(to_user_id: got_reaction_user_ids, from_user_id: current_user.id, status: 'like').map(&:to_user)
+    # @user = User.find(current.id)
+    @users = User.all
+  end
+
+  def second_index
+
+
   end
 end
